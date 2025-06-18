5b6b2a106faae25fabd3bc4333537717d77fb92d
Maccbrainy <iketakumichaelnedum@gmail.com>
2025-06-18 10:03:36 +0100

    custome types

---
+++ app/types/types.index.tsx
@@ -0,0 +1,19 @@
+export type ProductFeature = {
+    id:string
+    name:string
+    subTitle:string
+    summary:string
+    imageUrl:string
+    backgroundColor:string
+}
+
+export type Product = {
+    id:string
+    name:string
+    softwareName:string
+    subTitle:string
+    content:string
+    imageUrl:string
+    pathLink:string
+    backgroundColor:string
+}
