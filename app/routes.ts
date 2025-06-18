acd5b7d7d3bd0c2a58d5f1e6168507c7f5a4a631
Maccbrainy <iketakumichaelnedum@gmail.com>
2025-06-18 09:55:44 +0100

    route config updates+

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.route.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.route.tsx"),
+    route("core-school-management-solution", "routes/coreSchoolManagementSolution.route.tsx"),
+    route("fee-payments-and-management-solution","routes/feePaymentManagementSolution.route.tsx"),
+    route("academic-result-report-solution", "routes/academicResultReportSolution.route.tsx"),
+    route("communication-book", "routes/communicationBook.route.tsx")
+] satisfies RouteConfig;
