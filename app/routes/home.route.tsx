912ec39b517e60031427990f3100519d7a084db8
Maccbrainy <iketakumichaelnedum@gmail.com>
2025-06-18 09:56:57 +0100

    seo updates+

--- app/routes/home.route.tsx
+++ app/routes/home.route.tsx
@@ -1,13 +1,28 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { AcademflowPromisesHome, DoMoreWithLessHome, ExploreProductsHome, FortressLevelSecurityHome, OnboardOnAdvancePlatformHome, WelcomeHome } from "~/pages/home";
+import type { Route } from "./+types/home.route";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "Academflow - The Most Modern Education Management Solution for Schools, Colleges, Universities and Training Institutes" },
-    { name: "description", content: "All-in-one School Management Software. Streamline Education Management processes with a modern intuitive platform that leaves nothing out of the cycle; from student admission, onboarding, result processing, fee reconciliations to graduation." },
+    { title: "Academflow | All-in-One School Management Platform | The Most Modern School Management Software for Smart Schools" },
+    { name: "description", content: "Academflow simplifies school management processes in one modern powerful platform that leaves nothing out of the cycle. Cloud based, intuitive and secure - handle students' information system, disciplinary management, staff user's data system, fee payments and management system, result processing and communication." },
+    { name: "theme-color", content: "#31859c"},
+    { property: "og:title", content: "All-in-One School Management Software | Modern School Management Solution for Smart Schools" },
+    { property: "og:description", content: "Enterprise grade school management software designed for modern K–12 institutions, colleges, universities and training institutes. Manage everything from students' onboarding to graduation in one intuitive platform." },
+    { property: "og:type", content: "website"},
+    { property: "og:url", content: "https://academflow.com"},
+    { property: "og:image", content: "" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return(
+    <>
+      <WelcomeHome />
+      <ExploreProductsHome />
+      <DoMoreWithLessHome/>
+      <AcademflowPromisesHome/>
+      <FortressLevelSecurityHome/>
+      <OnboardOnAdvancePlatformHome/>
+    </>
+  );
 }
